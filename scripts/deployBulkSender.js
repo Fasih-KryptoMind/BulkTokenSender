e20fb29abe48cc6137f5706ce035736b89f99161
Fasih <fasih.haider@kryptomind.com>
2023-05-09 12:05:19 +0500

    Add deployment script

---
+++ scripts/deployBulkSender.js
@@ -0,0 +1,25 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const BulkSender = await hre.ethers.getContractFactory("BulkSender");
+  const bulkSender = await BulkSender.deploy();
+
+  await bulkSender.deployed();
+
+  console.log(
+    `BulkSender deployed to ${bulkSender.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
